55893e62e8265dc42372fb8c83442c167f304ca6
Moaz Baghdadi <e1228653@student.tuwien.ac.at>
2016-05-21 16:45:11 +0200

    implement conversion to json and db reading

--- DBconnection.hs
+++ DBconnection.hs
@@ -1,16 +1,89 @@
+
 module Main where
 
-import Database.PostgreSQL.Simple
-import Control.Monad
-import Control.Applicative
+import Database.HDBC.PostgreSQL
+import Database.HDBC
 
-main = do
-  conn <- connect defaultConnectInfo {
-    connectDatabase = "testDB"
-  }
+data Table = Table Season [Team]
 
-  putStrLn "2 + 2"
-  mapM_ print =<< ( query_ conn "select 2 + 2" :: IO [Only Int] )
-  
-LINE 1: CREATE TABLE test (id INTEGER NOT NULL, desc VARCHAR(80))
-                                                ^\n"}
+data Season = S Integer
+data Team = T String Points Goaldiff Logo
+data Points = P Integer
+data Goaldiff = D Integer
+data Logo = L String
+
+seasonQuery :: IO ()
+seasonQuery = 
+    do -- Connect to the database
+       conn <- connectPostgreSQL' "host=localhost dbname=testDB user=postgres password=postgres"
+
+       -- Run the query and store the results in r
+       r <- quickQuery' conn
+            "SELECT * from seasons"
+            []
+
+       -- Convert each row into a String
+       let stringRows = map convRow r
+                        
+       -- Print the rows out
+       mapM_ putStrLn stringRows
+
+       -- And disconnect from the database
+       disconnect conn
+
+    where convRow :: [SqlValue] -> String
+          convRow [sqlyear] = 
+              "season: " ++ show year
+              where year = (fromSql sqlyear)::Integer
+                    {-year = case fromSql year of
+                             Just x -> x
+                            Nothing -> "NULL"-}
+          convRow x = fail $ "Unexpected result: " ++ show x 
+
+teamQuery :: Integer -> IO ()
+teamQuery season = 
+    do -- Connect to the database
+       conn <- connectPostgreSQL' "host=localhost dbname=testDB user=postgres password=postgres"
+
+       -- Run the query and store the results in r
+       r <- quickQuery' conn
+            "select t.name, p.season, p.points, p.goaldiff, t.logo from team t join points p on t.name = p.team where season = ? order by (points, goaldiff) desc"
+            [toSql season]
+
+       -- Convert each row into a String
+       let stringRows = teamsToJSON $ map convRow r
+                        
+       -- Print the rows out
+       mapM_ putStrLn [stringRows]
+
+       -- And disconnect from the database
+       disconnect conn
+
+    where convRow :: [SqlValue] -> Team
+          convRow [sqlname, sqlseason, sqlpoints, sqlgoaldiff, sqllogo] = 
+              (T name (P points) (D goaldiff) (L logo))
+              where points = (fromSql sqlpoints)::Integer
+                    goaldiff = (fromSql sqlgoaldiff)::Integer
+                    season = (fromSql sqlseason)::Integer
+                    name = case fromSql sqlname of
+                             Just x -> x
+                             Nothing -> "NULL"
+                    logo = case fromSql sqllogo of
+                             Just x -> x
+                             Nothing -> "NULL"
+
+tableToJSON :: Table -> String
+tableToJSON (Table se teams) = "{\"season\" : \"" ++ x se ++ "\", \"table\" : [" ++ teamsToJSON teams ++ "] }"
+    where x (S se)= show se
+
+teamsToJSON :: [Team] -> String
+teamsToJSON [] = ""
+teamsToJSON ((T t p d l):ts) = "{ \"team\" : \"" ++ t ++ "\", \"points\" : \"" ++ getPoints p ++ "\", \"goaldiff\" : \"" ++ 
+                             getGoaldiff d ++ "\", \"logo\" : \"" ++ getLogo l ++ "\"}" ++ comma ++ teamsToJSON ts
+    where
+        getPoints (P x) = show x
+        getGoaldiff (D x) = show x
+        getLogo (L x) = x
+        comma = case ts of
+            []     -> ""
+            otherwise -> ", "
